ae409ff1247ebd822e959308b9f31512c998958e
Giorgos <lame.giorgos@gmail.com>
2023-05-25 00:00:55 +0300

    Minor

--- express/middleware/authJwt.js
+++ express/middleware/authJwt.js
@@ -13,9 +13,7 @@ verifyToken = (req, res, next) => {
 
   jwt.verify(token, config.secret, (err, decoded) => {
     if (err) {
-      return res.status(401).send({
-        message: "Unauthorized!"
-      });
+      return res.status(401).send(err);
     }
     req.userId = decoded.id;
     next();
