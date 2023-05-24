366ad7b32f78d672f885b3f13e51aeb8f1196342
Giorgos <lame.giorgos@gmail.com>
2023-05-24 19:30:16 +0300

    Sign in after sign up

--- express/auth/auth.controller.js
+++ express/auth/auth.controller.js
@@ -9,80 +9,99 @@ const Op = Sequelize.Op;
 const jwt = require("jsonwebtoken");
 const bcrypt = require("bcryptjs");
 
-exports.signup = (req, res) => {
-  // Save User to Database
-  User.create({
-    username: req.body.username,
-    email: req.body.email,
-    password: bcrypt.hashSync(req.body.password, 8)
-  })
-    .then(user => {
-      if (req.body.roles) {
-        Role.findAll({
-          where: {
-            name: {
-              [Op.or]: req.body.roles
-            }
+const signToken = (user) => {
+  return jwt.sign({ id: user.id }, config.secret, {
+    expiresIn: 86400 // 24 hours
+  });
+}
+
+const signup = async (req, res) => {
+
+  try {
+    // Save User to Database
+    const user = await User.create({
+      username: req.body.username,
+      email: req.body.email,
+      password: bcrypt.hashSync(req.body.password, 8)
+    })
+    if (req.body.roles) {
+      const roles = await Role.findAll({
+        where: {
+          name: {
+            [Op.or]: req.body.roles
           }
-        }).then(roles => {
-          user.setRoles(roles).then(() => {
-            res.send({ message: "User was registered successfully!" });
-          });
-        });
-      } else {
-        // user role = 1
-        user.setRoles([1]).then(() => {
-          res.send({ message: "User was registered successfully!" });
-        });
-      }
-    })
-    .catch(err => {
-      res.status(500).send({ message: err.message });
-    });
-};
-
-exports.signin = (req, res) => {
-  User.findOne({
-    where: {
-      email: req.body.email
-    }
-  })
-    .then(user => {
-      if (!user) {
-        return res.status(404).send({ message: "User Not found." });
-      }
-
-      const passwordIsValid = bcrypt.compareSync(
-        req.body.password,
-        user.password
-      );
-
-      if (!passwordIsValid) {
-        return res.status(401).send({
-          accessToken: null,
-          message: "Invalid Password!"
-        });
-      }
-
-      const token = jwt.sign({ id: user.id }, config.secret, {
-        expiresIn: 86400 // 24 hours
-      });
-
-      const authorities = [];
-      user.getRoles().then(roles => {
-        for (let i = 0; i < roles.length; i++) {
-          authorities.push("ROLE_" + roles[i].name.toUpperCase());
         }
-        res.status(200).send({
-          id: user.id,
-          username: user.username,
-          email: user.email,
-          roles: authorities,
-          accessToken: token
-        });
+      })
+      await user.setRoles(roles)
+      const token = signToken(user);
+      res.status(200).send({
+        id: user.id,
+        accessToken: token,
+        username: user.username,
+        email: user.email,
+        roles: roles,
       });
-    })
-    .catch(err => {
-      res.status(500).send({ message: err.message });
-    });
+
+
+    } else {
+      const roles = [1]
+      await user.setRoles(roles)
+      const token = signToken(user);
+      res.status(200).send({
+        id: user.id,
+        accessToken: token,
+        username: user.username,
+        email: user.email,
+        roles: roles,
+      });
+    }
+  } catch (err) {
+    res.status(500).send({ message: err.message });
+  }
 };
+
+const signin = async (req, res) => {
+  try {
+    const user = await User.findOne({
+      where: {
+        email: req.body.email
+      }
+    })
+
+    if (!user) {
+      return res.status(404).send({ message: "User Not found." });
+    }
+
+    const passwordIsValid = bcrypt.compareSync(
+      req.body.password,
+      user.password
+    );
+
+    if (!passwordIsValid) {
+      return res.status(401).send({
+        accessToken: null,
+        message: "Invalid Password!"
+      });
+    }
+
+    const token = signToken(user);
+
+    const authorities = [];
+    const roles = await user.getRoles()
+    for (let i = 0; i < roles.length; i++) {
+      authorities.push("ROLE_" + roles[i].name.toUpperCase());
+    }
+    res.status(200).send({
+      id: user.id,
+      username: user.username,
+      email: user.email,
+      roles: authorities,
+      accessToken: token
+    });
+  } catch (error) {
+    res.status(500).send({ message: error.message });
+  }
+};
+
+exports.signin = signin;
+exports.signup = signup;
